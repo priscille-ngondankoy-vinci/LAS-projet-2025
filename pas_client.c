d46df7d294d0a899a2dda09e1c30851f272f91ab
gauthier <alicia.gauthier@student.vinci.be>
2025-05-07 16:56:08 +0200

    ajout des pipes

--- pas_client.c
+++ pas_client.c
@@ -28,6 +28,31 @@ int main(int argc, char **argv){
   char pseudo[MAX_PSEUDO];
   union Message msg;
 
+  //création du pipe : écriture de pas-client et lecture de pas-cman-ipl
+  //création du pipe : écriture de pas-cman-ipl et lecture de pas-client
+  int pipe1[2], pipe2[2];
+  spipe(pipe1);
+  spipe(pipe2);
+
+  //processus fils pas-cman-ipl
+  pid_t pid = sfork();
+  if(pid == 0){
+
+    sdup2(pipe1[0], 0); // redirige l'entrée standard vers (une copie) du pipe de pas-client
+    sdup2(pipe2[1], 1); //redirige la sortie standard vers le pipe de pas-client
+    //ferme les descripteurs de pipe inutilisés puisque redirigés vers stdin et stdout
+    close(pipe1[0]); //fermé car en double
+    close(pipe1[1]); //inutilisé
+    close(pipe2[0]); //inutilisé
+    close(pipe2[1]); //fermé car en double
+    
+    //remplace le processus courant par pascman-ipl
+    sexecl("./target/release/pas-cman-ipl","pas-cman-ipl")
+  }
+
+  close(pipe1[0]);
+  close(pipe2[1]);
+
   /* retrieve player name */
   printf("Bienvenue dans le programe d'inscription au serveur de jeu\n");
   printf("Entrez votre pseudo :\n");
@@ -38,6 +63,7 @@ int main(int argc, char **argv){
   }
   pseudo[ret - 1] = '\0';
   msg.registration.msgt = REGISTRATION;
+  swrite(pipe1[1], &msg, sizeof(msg));
 
   int sockfd = initSocketClient(SERVER_IP, SERVER_PORT);
   swrite(sockfd, &msg, sizeof(msg));
