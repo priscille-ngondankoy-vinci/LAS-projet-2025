69ae2cc989bb2a4b307e0085cdb70c61e0c3cb8a
boubnan <hajar.boubnan@student.vinci.be>
2025-05-07 15:17:38 +0200

    suite pas_labos

--- pas_labo.c
+++ pas_labo.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include "utils_v3.h"
+#define SERVER_PORT 9090
 
 #define SERVER_EXEC "./pas_server"
 #define CLIENT_EXEC "./pas_client"
@@ -11,11 +13,13 @@
 int main() {
 
   pid_t server_pid, client1_pid, client2_pid;
+  char port_str[10];
+  sprintf(port_str, "%d", SERVER_PORT);
 
   // Démarrer le serveur
   server_pid = fork();
     if (server_pid == 0) {
-        execl(SERVER_EXEC, SERVER_EXEC, "9501", NULL);
+        sexecl(SERVER_EXEC, SERVER_EXEC, port_str, NULL);
         perror("Erreur lancement serveur");
         exit(1);
     }
@@ -27,7 +31,7 @@ int main() {
 
     client1_pid = fork();
     if (client1_pid == 0) {
-        execl(CLIENT_EXEC, CLIENT_EXEC, "127.0.0.1", "9501", NULL);
+        sexecl(CLIENT_EXEC, CLIENT_EXEC, "127.0.0.1", port_str, NULL);
         perror("Erreur lancement client 1");
         exit(1);
     }
@@ -36,12 +40,17 @@ int main() {
 
     client2_pid = fork();
     if (client2_pid == 0) {
-        execl(CLIENT_EXEC, CLIENT_EXEC, "127.0.0.1", "9501", NULL);
+        sexecl(CLIENT_EXEC, CLIENT_EXEC, "127.0.0.1", port_str, NULL);
         perror("Erreur lancement client 2");
         exit(1);
     }
     waitpid(client1_pid, NULL, 0);
     waitpid(client2_pid, NULL, 0);
 
+    kill(server_pid, SIGINT);
 
+    waitpid(server_pid, NULL, 0);
+
+    printf("Tous les processus sont terminés\n");
+    return 0;
 }
