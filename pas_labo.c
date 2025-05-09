c217e9ef1d80c4d4df90aaef4b51587f8a371e58
gauthier <alicia.gauthier@student.vinci.be>
2025-05-09 19:33:31 +0200

    pas labo

--- pas_labo.c
+++ pas_labo.c
@@ -1,56 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <signal.h>
+#include <fcntl.h>
 #include "utils_v3.h"
-#define SERVER_PORT 9090
-
-#define SERVER_EXEC "./pas_server"
-#define CLIENT_EXEC "./pas_client"
-
 
 int main(int argc, char *argv[]) {
-
-  pid_t server_pid, client1_pid, client2_pid;
-  char port_str[10];
-  sprintf(port_str, "%d", SERVER_PORT);
-
-  // Démarrer le serveur
-  server_pid = fork();
-    if (server_pid == 0) {
-        sexecl(SERVER_EXEC, SERVER_EXEC, port_str, NULL);
-        perror("Erreur lancement serveur");
-        exit(1);
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s <port> <map_file> <joueur1_file> <joueur2_file>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    sleep(10);
+    char *port = argv[1];
+    char *map_file = argv[2];
+    char *j1_file = argv[3];
+    char *j2_file = argv[4];
 
+    int pipe1[2], pipe2[2];
+    spipe(pipe1);
+    spipe(pipe2);
 
-    // lancement des 2 clients 
+    if (sfork() == 0)
+        sexecl("./pas_server", "pas_server", port, map_file, NULL);
 
-    client1_pid = fork();
-    if (client1_pid == 0) {
-        sexecl(CLIENT_EXEC, CLIENT_EXEC, "127.0.0.1", port_str, NULL);
-        perror("Erreur lancement client 1");
-        exit(1);
+    usleep(800000);
+
+    if (sfork() == 0) {
+        sdup2(pipe1[0], STDIN_FILENO);
+        sclose(pipe1[1]);
+        sexecl("./pas_client", "pas_client", "127.0.0.1", port, "-test", NULL);
+    }
+    sclose(pipe1[0]);
+
+    usleep(300000);
+
+    if (sfork() == 0) {
+        sdup2(pipe2[0], STDIN_FILENO);
+        sclose(pipe2[1]);
+        sexecl("./pas_client", "pas_client", "127.0.0.1", port, "-test", NULL);
+    }
+    sclose(pipe2[0]);
+
+    FILE *f1 = fopen(j1_file, "r");
+    FILE *f2 = fopen(j2_file, "r");
+    if (!f1 || !f2) {
+        perror("Erreur ouverture fichiers commandes");
+        sclose(pipe1[1]);
+        sclose(pipe2[1]);
+        exit(EXIT_FAILURE);
     }
 
-    sleep(10);
+    char c1, c2;
+    while (1) {
+        int r1 = fread(&c1, 1, 1, f1);
+        if (r1 == 1) swrite(pipe1[1], &c1, 1);
 
-    client2_pid = fork();
-    if (client2_pid == 0) {
-        sexecl(CLIENT_EXEC, CLIENT_EXEC, "127.0.0.1", port_str, NULL);
-        perror("Erreur lancement client 2");
-        exit(1);
+        int r2 = fread(&c2, 1, 1, f2);
+        if (r2 == 1) swrite(pipe2[1], &c2, 1);
+
+        if (r1 != 1 && r2 != 1) break;
+        usleep(100000);
     }
-    waitpid(client1_pid, NULL, 0);
-    waitpid(client2_pid, NULL, 0);
 
-    kill(server_pid, SIGINT);
+    usleep(3000000);
 
-    waitpid(server_pid, NULL, 0);
+    fclose(f1);
+    fclose(f2);
+    sclose(pipe1[1]);
+    sclose(pipe2[1]);
 
-    printf("Tous les processus sont terminés\n");
     return 0;
 }
