4627c7fe376909fd02651a7983ef856b4ebc0608
coita <larisa.coita@student.vinci.be>
2025-05-09 16:23:07 +0200

    CA BOUGEEEEE

--- pas_client.c
+++ pas_client.c
@@ -1,114 +1,137 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <errno.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
 
 #include "utils_v3.h"
 #include "pascman.h"
+#include "game.h"
 
-void launch_graphical_interface() {
-    sexecl("./target/release/pas-cman-ipl", "pas-cman-ipl", NULL);
+#define SERVER_PORT 9090
+#define SERVER_IP "127.0.0.1"
+#define KEY 19753
+#define MAX_PSEUDO 256
+#include <termios.h>
+#include <unistd.h>
+
+// Active le mode raw (pas de buffer, pas d’echo)
+void enable_raw_mode() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <server_ip> <server_port>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    char *server_ip = argv[1];
-    int server_port = atoi(argv[2]);
-
-    int sockfd = ssocket();
-    sconnect(server_ip, server_port, sockfd);
-
-    // Création du pipe : direction clavier sera écrite ici par pas-cman-ipl
-    int pipefd[2];
-    spipe(pipefd);
-
-    pid_t pid = sfork();
-    if (pid == 0) {
-        // Enfant : lance pas-cman-ipl
-
-        // Rediriger STDIN vers sockfd (messages serveur)
-        sdup2(sockfd, STDIN_FILENO);
-
-        // Rediriger STDOUT vers pipefd[1] (direction vers client)
-        sdup2(pipefd[1], STDOUT_FILENO);
-
-        // Nettoyage
-        sclose(pipefd[0]);
-        sclose(pipefd[1]);
-        sclose(sockfd);
-
-        // Lancement
-        launch_graphical_interface();
-        exit(EXIT_FAILURE);  // sécurité
-    }
-
-    // Parent : pas_client
-    sclose(pipefd[1]); // lecture uniquement
-
-    // Inscription
-    union Message msg;
-    ssize_t r = sread(sockfd, &msg, sizeof(msg));
-    if (r != sizeof(msg) || msg.msgt != REGISTRATION) {
-        fprintf(stderr, "Erreur : échec de l'inscription.\n");
-        sclose(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("✅ Connecté avec succès en tant que joueur %u !\n", msg.registration.player);
-
-
-// 2. Boucle attente : on attend que le serveur envoie SPAWN (début du jeu)
-bool start_game = false;
-while (!start_game) {
-    union Message init_msg;
-    if (read(sockfd, &init_msg, sizeof(init_msg)) <= 0) break;
-
-    if (init_msg.msgt == SPAWN) {
-        start_game = true;
-
-        // Fork et lancement de l’interface ici
-        pid_t pid = sfork();
-        if (pid == 0) {
-            sdup2(sockfd, STDIN_FILENO);
-            sdup2(pipefd[1], STDOUT_FILENO);
-            sclose(pipefd[0]); sclose(pipefd[1]); sclose(sockfd);
-            launch_graphical_interface();
-        } else {
-            sclose(pipefd[1]);
-            nwrite(STDOUT_FILENO, &init_msg, sizeof(init_msg)); // envoie le premier SPAWN à l’interface
-        }
-    }
+// Remet le terminal en mode normal
+void disable_raw_mode() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag |= (ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
-    // Boucle principale
-    bool game_over = false;
-    while (!game_over) {
-        // Lire direction depuis pas-cman-ipl
-        enum Direction dir;
-        ssize_t dr = read(pipefd[0], &dir, sizeof(dir));
-        if (dr == sizeof(dir)) {
-            swrite(sockfd, &dir, sizeof(dir));
-        }
 
-        // Lire message serveur
-        union Message msg_in;
-        ssize_t sr = read(sockfd, &msg_in, sizeof(msg_in));
-        if (sr == sizeof(msg_in)) {
-            nwrite(STDOUT_FILENO, &msg_in, sizeof(msg_in));  // transmis à pas-cman-ipl
-            if (msg_in.msgt == GAME_OVER) {
-                game_over = true;
-            }
-        }
+int initSocketClient(char * serverIP, int serverPort)
+{
+  int sockfd = ssocket();
+  sconnect(serverIP, serverPort, sockfd);
+  return sockfd;
+}
 
-        usleep(5000);
+void run_pas_cman_ipl(void *arg_sockfd, void *arg_pipe_write) {
+  int sockfd = *(int *)arg_sockfd;
+  int pipe_write = *(int *)arg_pipe_write;
+
+  // Rediriger stdin vers la socket (pour que pas-cman-ipl lise les messages depuis le serveur)
+  sdup2(sockfd, 0);         
+  // Rediriger stdout vers le pipe (pour que pas-cman-ipl écrive ses messages dans le pipe)
+  sdup2(pipe_write, 1);  
+
+  close(sockfd);
+  close(pipe_write);
+
+  sexecl("./target/release/pas-cman-ipl", "pas-cman-ipl", NULL);
+}
+
+int main(int argc, char **argv){
+  uint32_t player_id;
+  char pseudo[MAX_PSEUDO];
+  union Message msg;
+  int sockfd = initSocketClient(SERVER_IP, SERVER_PORT);
+
+  // Création du pipe : écriture par pas-cman-ipl (stdout), lecture par pas-client
+  int pipe[2];
+  spipe(pipe);
+
+  int psockfd = sockfd;
+  int ppipe_write = pipe[1];
+
+  fork_and_run2(run_pas_cman_ipl, &psockfd, &ppipe_write);
+
+  close(pipe[1]); 
+
+  /* retrieve player name */
+  printf("Bienvenue dans le programme d'inscription au serveur de jeu\n");
+  printf("Entrez votre pseudo :\n");
+  int ret = sread(0, pseudo, MAX_PSEUDO);
+  if (ret < 2) {
+    printf("Erreur: vous devez entrer un nom de joueur\n");
+    exit(1);
+  }
+  pseudo[ret - 1] = '\0';
+
+  // Envoi du message d'enregistrement au serveur
+  msg.registration.msgt = REGISTRATION;
+  swrite(sockfd, &msg, sizeof(msg));
+  sread(sockfd, &msg, sizeof(msg));
+  player_id = msg.registration.player;
+  // Affichage du numéro de joueur
+  printf("Je suis le joueur numéro %u\n", msg.registration.player);
+
+ while (1) {
+    enum Direction dir;
+    ssize_t ret = sread(pipe[0], &dir, sizeof(dir));
+    if (ret <= 0) break;
+
+    msg.movement.msgt = MOVEMENT;
+    msg.movement.id = player_id;
+
+    // Traduction de la direction en mouvement
+    switch (dir) {
+        case LEFT:
+            msg.movement.pos.x = -1;
+            msg.movement.pos.y = 0;
+            break;
+        case RIGHT:
+            msg.movement.pos.x = 1;
+            msg.movement.pos.y = 0;
+            break;
+        case UP:
+            msg.movement.pos.x = 0;
+            msg.movement.pos.y = -1;
+            break;
+        case DOWN:
+            msg.movement.pos.x = 0;
+            msg.movement.pos.y = 1;
+            break;
+        default:
+            continue; // Ne rien envoyer si direction inconnue
     }
 
-    sclose(pipefd[0]);
-    sclose(sockfd);
-    return EXIT_SUCCESS;
+    swrite(sockfd, &msg, sizeof(msg));
+}
+
+    disable_raw_mode();
+
+
+  sclose(sockfd);
+  close(pipe[0]);
+
+  return 0;
 }
